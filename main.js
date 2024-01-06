35c19e55b12143519f9b489141c4a69627397981
Quma4ka <144566868+Quma4ka@users.noreply.github.com>
2024-01-07 00:17:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById('q_1').addEventListener('click', function () {
+    var container = document.getElementById('a_1');
+    if (container.style.display === 'none') {
+        container.style.display = 'block';
+    } else {
+        container.style.display = 'none';
+    }
+});
+
+document.getElementById('q_2').addEventListener('click', function () {
+    var container = document.getElementById('a_2');
+    if (container.style.display === 'none') {
+        container.style.display = 'block';
+    } else {
+        container.style.display = 'none';
+    }
+});
+
+document.getElementById('q_3').addEventListener('click', function () {
+    var container = document.getElementById('a_3');
+    if (container.style.display === 'none') {
+        container.style.display = 'block';
+    } else {
+        container.style.display = 'none';
+    }
+});
+
+
